07068728ebcc531ad692e07585dbf1cb772d1f7d
wangzhengshuai <kingkadienm@163.com>
2022-04-13 22:54:46 +0800

    [feature] post json传参

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.bean.User;
+import com.example.demo.bean.request.ReceiveBean;
+import com.example.demo.bean.response.ResponseBean;
 import com.example.demo.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,22 +22,37 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+    @Resource
+    private ObjectMapper objectMapper;
 
-
-    @RequestMapping("/getAllUser")
+    @RequestMapping(value = "/getAllUser", method = RequestMethod.GET)
     @ResponseBody
-    public List<User> findAll() {
+    public ResponseBean findAll() {
+        ResponseBean responseBean = new ResponseBean();
         List<User> list = new ArrayList<>();
         list = userRepository.findAll();
+        responseBean.setCode(200);
+        responseBean.setMessage("success");
+        responseBean.setResult(list);
         System.out.println("================");
-        return list;
+        return responseBean;
     }
 
-    @RequestMapping("/getByUserName")
+
+    @PostMapping(value = "/getByUserName",
+            consumes = "application/json"
+//            headers = "User-Agent=Android"
+    )
     @ResponseBody
-    public User getByUserName(String userName) {
+    public ResponseBean getByUserName(@RequestBody ReceiveBean receiveBean) {
+        String userName = receiveBean.getUserName();
+        System.out.println(userName);
         User user = userRepository.findUserByUserName(userName);
-        return user;
+        ResponseBean responseBean = new ResponseBean();
+        responseBean.setCode(200);
+        responseBean.setMessage("success");
+        responseBean.setResult(user);
+        return responseBean;
     }
 
 }
